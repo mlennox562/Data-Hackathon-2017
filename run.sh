a4122fd047e8e57bd6f8f2c5e2c84c4411a9cebe
Mark <mlennox562@hotmail.co.uk>
2017-11-18 23:39:41 +0000

    first commit

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#chmod +x run.sh
+#./run.sh
+
+
+prepare() {
+  echo "Preparing $1..."
+  python prepare-$1.py
+}
+
+train() {
+  echo "Training $1..."
+  time python -u train.py --threads 4 $1 | tee logs/$1.log
+}
+
+
+# Prepare features
+
+#prepare basic
+#prepare numeric-boxcox
+#prepare numeric-scaled
+#prepare numeric-rank-norm
+#prepare categorical-encoded
+#prepare categorical-counts
+
+#prepare categorical-dummy #Leave this script out.
+#prepare svd #Leave this script out.
+
+## Basic models
+#train lr-ce
+#
+#train et-ce
+#train rf-ce
+#train gb-ce
+#
+## LibFM
+#train libfm-ce #Don't have it installed!
+#
+## LightGBM
+#train lgb-ce
+#
+## XGB
+train xgb-ce
+#train xgb-ce-2
+#
+#train xgbf-ce
+#train xgbf-ce-2
